5c809bcd4abf060026aae21d7245ead545a06244
Marko Korhonen <reekymarko@reekynet.com>
2017-06-21 19:56:07 +0300

    switched from wget to curl

--- update.sh
+++ update.sh
@@ -4,16 +4,16 @@
 
 domain="***YOUR DOMAIN***"
 updateurl="***YOUR UPDATE URL***"
-# You can get this by going to http://freedns.afraid.org/dynamic/
+# You can get this by going to http://freedns.afraid.org/dynamic/v2/
 # and copying the "Direct URL" link
 
 ###################################################################
 
 registered=$(nslookup $domain|tail -n2|grep A|sed s/[^0-9.]//g)
-current=$(wget -q -O - http://checkip.dyndns.org|sed s/[^0-9.]//g)
+current=$(curl -s http://checkip.dyndns.org|sed s/[^0-9.]//g)
 
 if [ "$current" != "$registered" ]; then                           
-	wget -q -O /dev/null $updateurl
+	curl -s >> /dev/null $updateurl
 	echo "The DNS record of $domain was updated to $current from $registered"
 else
 	echo "DNS record up to date"
